dc687835c8aa07ba0c14a89c8fb20b54ea042f0f
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-08-12 18:30:58 +0530

    Added nested recursive forum :recycle:

--- app/src/pages/api/forumDat.ts
+++ app/src/pages/api/forumDat.ts
@@ -1,10 +1,16 @@
 const forumArray = [
   {
-    question:
-      "ERROR EPERM BLEH BLEH BLEH BLEH BLEH BLEH BLEH BLEH Lorem Ipsum Dolor Sit Amet Consectetur Adipiscing Elit Mattis Eros Vivamus Euismod Nisl Quisque BLEH BLEH BLEH BLEH",
+    msg: "ERROR EPERM BLEH BLEH BLEH BLEH BLEH BLEH BLEH BLEH Lorem Ipsum Dolor Sit Amet Consectetur Adipiscing Elit Mattis Eros Vivamus Euismod Nisl Quisque BLEH BLEH BLEH BLEH",
     replies: [
       {
         msg: "Lorem Ipsum dolor sit amet, consectetur adipiscing el",
+        replies: [
+          {
+            msg: "The billboard of the billboard is under the hood. The billboard should be a famous mauris.",
+            upvotes: 0,
+            downvotes: 0,
+          },
+        ],
         upvotes: 5,
         downvotes: 1,
       },
@@ -24,7 +30,8 @@ const forumArray = [
         downvotes: 1,
       },
     ],
-    markedCorrect: "I AM CORRECT AS PER AUTHOR = > Lorem Ipsum dolor sit amet, consectetur adipiscing el",
+    markedCorrect:
+      "I AM CORRECT AS PER AUTHOR = > Lorem Ipsum dolor sit amet, consectetur adipiscing el",
     questionId: "099099",
     upvotes: 101,
     downvotes: 39,
