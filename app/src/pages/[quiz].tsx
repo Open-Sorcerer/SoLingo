bfcb570a48c34e2bb455b74d38bb25d6e847a48d
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-08-14 02:03:09 +0530

    noob stuff go brrr :alien:

--- app/src/pages/[quiz].tsx
+++ app/src/pages/[quiz].tsx
@@ -1,22 +1,22 @@
 import type { NextPage } from "next";
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Head from "next/head";
 import { useState } from "react";
 import quizJson from "./api/quiz.json";
 
 const Quests: NextPage = (props) => {
   //Takes query from router and uses it call specific question json.
-  const router = useRouter()
+  const router = useRouter();
   let { quiz } = router.query;
   if (Array.isArray(quiz)) {
-    quiz = quiz.join('');
+    quiz = quiz.join("");
   }
-  console.log(quiz)
+  console.log(quiz);
   const no = parseInt(quiz) - 1;
-  console.log(no)
-  let questions = quizJson[1][1]
-  
+  console.log(no);
+  let questions = quizJson[1][1];
+
   //These are used to maintain question and score state.
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedOptions, setSelectedOptions] = useState([]);
@@ -55,7 +55,6 @@ const Quests: NextPage = (props) => {
     setShowScore(true);
   };
 
-
   return (
     <div>
       <Head>
@@ -67,29 +66,29 @@ const Quests: NextPage = (props) => {
           <div className="flex flex-col justify-center items-center p-3">
             <div className="text-xl font-bold mb-3">Quest</div>
             {/* If showScore is true then we will display result or else we will display questions */}
-            { showScore ? (
-            <>
-              <h1 className="text-3xl font-semibold text-center text-white">
-                You scored {score} out of {questions.length}
-              </h1>
-              {/* If the score is less then 8 then you can't upload the result */}
-              { (score < 8) ? (
+            {showScore ? (
+              <>
+                <h1 className="text-3xl font-semibold text-center text-white">
+                  You scored {score} out of {questions.length}
+                </h1>
+                {/* If the score is less then 8 then you can't upload the result */}
+                {score < 8 ? (
                   <h1 className="text-3xl font-semibold text-center text-white">
                     You didn't pass this round.
                   </h1>
                 ) : (
-                  <button onClick={handlePrevious}
-                    className="w-[49%] py-3 bg-indigo-600 rounded-lg">
+                  <button
+                    onClick={handlePrevious}
+                    className="w-[49%] py-3 bg-indigo-600 rounded-lg"
+                  >
                     Upload
                   </button>
-                )
-              }
-              <Link href="/quest">
+                )}
+                <Link href="/quest">
                   <a className="w-[49%] py-3 bg-indigo-600 rounded-lg">Retry</a>
-              </Link>
-            </>
-            ) : 
-            (
+                </Link>
+              </>
+            ) : (
               <>
                 <div className="flex flex-col items-start w-full">
                   <h4 className="mt-10 text-xl text-white/60">
@@ -99,7 +98,8 @@ const Quests: NextPage = (props) => {
                     {questions[currentQuestion].question}
                   </div>
                   <div className="flex flex-col w-full">
-                    {questions[currentQuestion].answerOptions.map((answer, index) => (
+                    {questions[currentQuestion].answerOptions.map(
+                      (answer, index) => (
                         <div
                           key={index}
                           className="flex items-center w-full py-4 pl-5 m-2 ml-0 space-x-2 border-2 cursor-pointer border-white/10 rounded-xl bg-white/5"
@@ -111,15 +111,15 @@ const Quests: NextPage = (props) => {
                             value={answer.answer}
                             onChange={(e) => handleAnswerOption(answer.answer)}
                             checked={
-                              answer.answer === selectedOptions[currentQuestion]?.answerByUser
+                              answer.answer ===
+                              selectedOptions[currentQuestion]?.answerByUser
                             }
                             className="w-6 h-6 bg-black"
                           />
                           <p className="ml-6 text-white">{answer.answer}</p>
                         </div>
-                      ))
-                    }
-
+                      )
+                    )}
                   </div>
                   <div className="flex justify-between w-full mt-4 text-white">
                     <button
@@ -130,24 +130,25 @@ const Quests: NextPage = (props) => {
                     </button>
                     <button
                       onClick={
-                        currentQuestion + 1 === questions.length ? handleSubmitButton : handleNext
+                        currentQuestion + 1 === questions.length
+                          ? handleSubmitButton
+                          : handleNext
                       }
                       className="w-[49%] py-3 bg-indigo-600 rounded-lg"
                     >
-                      {currentQuestion + 1 === questions.length ? "Submit" : "Next"}
+                      {currentQuestion + 1 === questions.length
+                        ? "Submit"
+                        : "Next"}
                     </button>
                   </div>
                 </div>
-
               </>
-            )
-            }
+            )}
           </div>
         </div>
       </div>
     </div>
   );
-
 };
 
 export default Quests;
