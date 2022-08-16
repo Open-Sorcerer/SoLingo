254c526edd3c64f59f250244a43946c42cdeb79c
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-08-16 19:10:48 +0530

    replyThread to be added :grey_question:

--- app/src/views/forum/index.tsx
+++ app/src/views/forum/index.tsx
@@ -156,17 +156,17 @@ export const ForumView = ({}) => {
               id="title"
               placeholder="Title"
               className={
-                "w-full flex flex-col justify-center items-start bg-transparent border border-gray-300 rounded-t-lg px-5 py-1"
+                "w-full flex flex-col justify-center items-start bg-transparent border border-white/20 rounded-t-lg px-5 py-1"
               }
             />
             <textarea
               id="description"
               placeholder="Description"
               className={
-                "w-full flex flex-col justify-center items-start bg-transparent border border-gray-300 py-2 px-5"
+                "w-full flex flex-col justify-center items-start bg-transparent border border-white/20 py-2 px-5"
               }
             />
-            <div className="w-full flex flex-row justify-between items-center bg-transparent border border-gray-300 rounded-b-lg">
+            <div className="w-full flex flex-row justify-between items-center bg-transparent border border-white/20 rounded-b-lg">
               <input
                 title="upload"
                 type="file"
